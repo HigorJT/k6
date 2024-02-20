4d0f3072d95b1d5d2497cf7988c25d62ddea7f08
Higor Jesus <higorjesus@Higors-MacBook-Air.local>
2024-02-20 15:53:29 -0300

    report workflows

--- modulo5/grafana.js
+++ modulo5/grafana.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import {sleep, check} from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     stages: [
@@ -29,3 +30,9 @@ export default function(){
     });
     sleep(1);
 }
+
+export function handleSummary(data) {
+    return {
+      "test.html": htmlReport(data),
+    };
+  }
